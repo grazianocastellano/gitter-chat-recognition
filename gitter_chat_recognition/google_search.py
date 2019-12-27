25ec04034c1abfa4de3a9a3725db1ca712363274
Graziano Castellano <grazianocastellano@live.it>
2019-12-27 16:57:40 +0100

    add function for google search

---
+++ gitter_chat_recognition/google_search.py
@@ -0,0 +1,19 @@
+from googlesearch import search
+import pprint
+
+
+def google_search(query_test):
+    query = 'gitter '+ query_test
+    my_results_list = []
+    for i in search(query,    # The query you want to run
+                tld = 'com',  # The top level domain
+                lang = 'en',  # The language
+                num = 10,     # Number of results per page
+                start = 0,    # First result to retrieve
+                stop = 5,  # Last result to retrieve
+                pause = 2.0,  # Lapse between HTTP requests
+               ):
+        my_results_list.append(i)
+        print(i)
+
+google_search('film')
