e201af29a8a5cf4e72d8f0ba3449d2c4732aa8c9
Graziano Castellano <grazianocastellano@live.it>
2019-12-30 15:43:53 +0100

    add test package

---
+++ test_gitter_chat_recognition/test_automatic_recognition.py
@@ -0,0 +1,21 @@
+import unittest
+
+from gitter_chat_recognition.automatic_recognition import \
+    gitter_chat_full_project_name as gitter_chat_full_project_name
+from gitter_chat_recognition.automatic_recognition import \
+    gitter_chat_project_name
+
+
+class Test_Automatic_Recognition(unittest.TestCase):
+
+    def test_gitter_chat_full_project_name(self):
+        self.assertEquals(gitter_chat_full_project_name('JabRef/jabref'),
+                          'https://gitter.im/JabRef/jabref')
+
+    def test_gitter_chat_project_name(self):
+        self.assertEquals(gitter_chat_project_name('vscode'),
+                          None)
+
+
+if __name__ == '__main__':
+    unittest.main()
