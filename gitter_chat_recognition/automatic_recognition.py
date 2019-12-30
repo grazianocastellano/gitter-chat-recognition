bc053d56eaea5a13cc602ec7c100a2984d337650
Graziano Castellano <grazianocastellano@live.it>
2019-12-30 15:42:00 +0100

    minor edit

--- gitter_chat_recognition/automatic_recognition.py
+++ gitter_chat_recognition/automatic_recognition.py
@@ -23,19 +23,20 @@ def get_project_name(org):
 def gitter_chat_full_project_name(project_name):
     url = 'https://gitter.im/' + project_name
     resp = requests.get(url)
+    print(url)
     if resp.status_code != 200:
-        return url
-    else:
         return None
+    else:
+        return url
 
 
 def gitter_chat_project_name(project_name):
     url = 'https://gitter.im/' + project_name + "/" + project_name
     resp = requests.get(url)
     if resp.status_code != 200:
-        return url
-    else:
         return None
+    else:
+        return url
 
 
 '''get_room_link('rails')
