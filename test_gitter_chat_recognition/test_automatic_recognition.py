486df1cc189eb4748b8a26294c31c006db634f05
Graziano Castellano <grazianocastellano@live.it>
2019-12-30 16:35:49 +0100

    add test for get project_name

--- test_gitter_chat_recognition/test_automatic_recognition.py
+++ test_gitter_chat_recognition/test_automatic_recognition.py
@@ -3,7 +3,8 @@ import unittest
 from gitter_chat_recognition.automatic_recognition import \
     gitter_chat_full_project_name as gitter_chat_full_project_name
 from gitter_chat_recognition.automatic_recognition import \
-    gitter_chat_project_name
+    gitter_chat_project_name as gitter_chat_project_name
+from gitter_chat_recognition.automatic_recognition import get_project_name
 
 
 class Test_Automatic_Recognition(unittest.TestCase):
@@ -15,6 +16,10 @@ class Test_Automatic_Recognition(unittest.TestCase):
     def test_gitter_chat_project_name(self):
         self.assertEqual(gitter_chat_project_name('vscode'),
                          None)
+
+    def test_get_project_name(self):
+        list1 = ['gitter-chat-recognition', 'GitterPy', 'grimoirelab-perceval']
+        self.assertEqual(get_project_name('grazianocastellano'), list1)
 
 
 if __name__ == '__main__':
