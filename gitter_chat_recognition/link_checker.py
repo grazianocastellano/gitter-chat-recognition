cdbdd423998a48c112a55c9e0a6cad5f1060b3c1
Graziano Castellano <grazianocastellano@live.it>
2020-01-07 15:49:23 +0100

    syntax correction

--- gitter_chat_recognition/link_checker.py
+++ gitter_chat_recognition/link_checker.py
@@ -3,14 +3,10 @@ import requests
 
 def link_checker(url, project_name):
     html = requests.get(url).text.split()
-    finder = 'github.com/' + project_name 
+    finder = 'github.com/' + project_name
     for elem in html:
-        if elem.find(finder)!= -1:
+        if elem.find(finder) != -1:
             list = elem.split('href="')
             link_github = list[1].split('"')[0]
             return(link_github)
     return None
-            
-
-
-print(link_checker('https://gitter.im/flutter/flutter','flutter/flutter'))
