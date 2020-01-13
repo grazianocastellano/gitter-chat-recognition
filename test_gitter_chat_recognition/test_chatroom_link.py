0d260e1739104c7828f804421dc5670d26ddc25d
Graziano Castellano <grazianocastellano@live.it>
2020-01-13 13:01:10 +0100

    add new test for link_retrevier

--- test_gitter_chat_recognition/test_chatroom_link.py
+++ test_gitter_chat_recognition/test_chatroom_link.py
@@ -12,6 +12,10 @@ class Test_Chatroom_link(unittest.TestCase):
     def test_chatroom_link1(self):
         self.assertIsNone(link_retrevier('atom/atom', 'atom'))
 
+    def test_chatroom_link2(self):
+        self.assertEqual(link_retrevier('microsoft/TypeScript', 'TypeScript'),
+                         'https://gitter.im/microsoft/TypeScript')
+
 
 if __name__ == "__main__":
     unittest.main()
