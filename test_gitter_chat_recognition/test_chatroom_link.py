30b91ffe26d792bdd8dc12dd17b34a6de857eeed
Graziano Castellano <grazianocastellano@live.it>
2020-01-13 12:42:13 +0100

    add test for link retrevier

---
+++ test_gitter_chat_recognition/test_chatroom_link.py
@@ -0,0 +1,16 @@
+import unittest
+from gitter_chat_recognition.chatroom_link import link_retrevier
+
+
+class Test_Chatroom_link(unittest.TestCase):
+
+    def test_chatroom_link(self):
+        self.assertEqual(link_retrevier('flutter/flutter','flutter'),
+                         'flutter/flutter')
+
+    def test_chatroom_link1(self):
+        self.assertIsNone(link_retrevier('atom/atom','atom'))
+
+
+if __name__ == "__main__":
+    unittest.main()
