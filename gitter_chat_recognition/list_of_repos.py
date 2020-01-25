fafe9620bafc2a07d5ae938a8f42beff382b9d70
Graziano Castellano <grazianocastellano@live.it>
2020-01-25 17:47:37 +0100

    edited metod fror get_project_name edit

--- gitter_chat_recognition/list_of_repos.py
+++ gitter_chat_recognition/list_of_repos.py
@@ -4,8 +4,11 @@ from gitter_chat_recognition.chatroom_link import link_retrevier
 
 def list_of_repos(org_name):
     list = get_project_name(org_name)
-    for elem in list:
-        print(elem)
-        gitter_link = link_retrevier(elem, elem.split('/')[1])
-        if(gitter_link) is not None:
-            print(gitter_link)
+    try:
+        for elem in list:
+            print(elem)
+            gitter_link = link_retrevier(elem, elem.split('/')[1])
+            if(gitter_link) is not None:
+                print(gitter_link)
+    except:
+        print('Users not Found')
