37b3229c6f334efeea1f878ff56edd20ffb607bd
Graziano Castellano <grazianocastellano@live.it>
2020-01-27 09:33:55 +0100

    add control for github page

--- gitter_chat_recognition/link_checker.py
+++ gitter_chat_recognition/link_checker.py
@@ -20,7 +20,9 @@ def link_checker(url, project_name):
 
 def link_checker_beta(url, project_name):
     gitter_id = url.split('gitter.im/')[1]
-    resp = requests.get('https://github.com/' + gitter_id)
+    github_url = 'https://github.com/' + gitter_id
+    github_project = 'https://github.com/'+project_name
+    resp = requests.get(github_url)
     if resp.status_code != 200:
         # TODO
         # Aggiungere controllo tra nome della lobby e nome del progetto
@@ -30,13 +32,15 @@ def link_checker_beta(url, project_name):
         for elem in split_project_name:
             for elem2 in split_gitter_name:
                 if elem2.find(elem) != -1:
-                    temp = True
+                    resp2 = requests.get(github_project)
+                    if resp2.status_code == 200:
+                        temp = True
         if(temp is True):
-            return url
+            return github_project
         else:
             return None
     else:
         html = resp.text.split()
         for elem in html:
-            if elem.find('https://github.com/'+project_name):
-                return url
+            if elem.find(github_project) != -1:
+                return github_project
