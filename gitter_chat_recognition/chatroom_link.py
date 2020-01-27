905770d2bd4dc6dd931a0a26b84455ce4b62c51f
Graziano Castellano <grazianocastellano@live.it>
2020-01-27 10:08:59 +0100

    removed gitter chat project name use

--- gitter_chat_recognition/chatroom_link.py
+++ gitter_chat_recognition/chatroom_link.py
@@ -3,7 +3,7 @@ from gitter_chat_recognition.automatic_recognition import \
 from gitter_chat_recognition.automatic_recognition import \
     gitter_chat_full_project_name as gitter_chat_full_project_name
 from gitter_chat_recognition.automatic_recognition import \
-    gitter_chat_project_name as gitter_chat_project_name
+    reading_contributing as reading_contributing
 from gitter_chat_recognition.google_search import google_search
 
 
@@ -11,10 +11,10 @@ def link_retrevier(full_project_name, name_project):
     link_return = gitter_badge(full_project_name)
     if link_return is not None:
         return link_return
+    elif reading_contributing(full_project_name) is not None:
+        return reading_contributing(full_project_name)
     elif gitter_chat_full_project_name(full_project_name) is not None:
         return gitter_chat_full_project_name(full_project_name)
-    elif gitter_chat_project_name(name_project) is not None:
-        return gitter_chat_project_name(name_project)
     elif google_search(full_project_name) is not None:
         return google_search(full_project_name)
     else:
