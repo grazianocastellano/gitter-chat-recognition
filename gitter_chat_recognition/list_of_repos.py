70ab60c0f6e888fe8607541be7ca47721b851758
Graziano Castellano <grazianocastellano@live.it>
2020-01-30 16:09:42 +0100

    add chance to save list of repos in a matrix

--- gitter_chat_recognition/list_of_repos.py
+++ gitter_chat_recognition/list_of_repos.py
@@ -4,11 +4,22 @@ from gitter_chat_recognition.chatroom_link import link_retrevier
 
 def list_of_repos(org_name):
     list = get_project_name(org_name)
+    matrix = []
     try:
         for elem in list:
             print(elem)
             gitter_link = link_retrevier(elem, elem.split('/')[1])
             if(gitter_link) is not None:
                 print(gitter_link)
+                matrix.append([elem, gitter_link])
+            else:
+                matrix.append([elem, None])
+        print(matrix)
+        with open(org_name + '.txt', "w") as openfile:
+            openfile.write(matrix)
+        openfile.close()
     except:
         print('Users not Found')
+
+
+list_of_repos('flutter')
